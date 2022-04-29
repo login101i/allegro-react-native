46ede79161baedf0104bbf92faad000dbbf51aa5
Maciej <maciejkruszyniak@gmail.com>
2022-04-29 23:05:36 +0200

    update

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose')
-
-
+const mongoose = require("mongoose");
 
 const connectDataBase = () => {
-    mongoose
-			.connect(process.env.MONGO_URI, {
-				useUnifiedTopology: true,
-				useNewUrlParser: true,
-				useCreateIndex: true,
-			})
-			.then((con) => {
-				console.log(
-					`MongoDB połączone z cluster em: ${con.connection.host}`.magenta.underline
-				);
-			});
-}
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB połączone z cluster em: ${con.connection.host}`.magenta
+          .underline
+      );
+    });
+};
 
-module.exports = connectDataBase
+module.exports = connectDataBase;
--- Backend/server.js
+++ Backend/server.js
@@ -6,41 +6,41 @@ const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 const colors = require("colors");
 
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "config/config.env" });
 
 // Handle Uncaught exceptions
 process.on("uncaughtException", (err) => {
-	console.log(`ERROR: ${err.stack}`);
-	console.log(
-		"Wyłączam server bo pojawił się w app.js uncaught exception, np wprowadziłeś zmienną, która nie jest zdefiniowana."
-	);
-	process.exit(1);
+  console.log(`ERROR: ${err.stack}`);
+  console.log(
+    "Wyłączam server bo pojawił się w app.js uncaught exception, np wprowadziłeś zmienną, która nie jest zdefiniowana."
+  );
+  process.exit(1);
 });
 
 connectDatabase();
 
 // Wpisujemy gdy tworzymy komponenty na frontEndzie np component Register
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const server = app.listen(process.env.PORT, () => {
-	console.log(
-		`Server działa na porcie ${process.env.PORT} w statusie ${process.env.NODE_ENV} `
-			.green.bold
-	);
+  console.log(
+    `Server działa na porcie ${process.env.PORT} w statusie ${process.env.NODE_ENV} `
+      .green.bold
+  );
 });
 
 // Handle Unhandled Promise rejections
 process.on("unhandledRejection", (err) => {
-	console.log(`ERROR: ${err.message}`);
-	console.log(
-		"Wyłączono server bo jest unhandled promise rejections, np w adresie do mongoDB jest literówka.."
-	);
-	server.close(() => {
-		process.exit(1);
-		// cg czemu 1 to nie wiem
-	});
+  console.log(`ERROR: ${err.message}`);
+  console.log(
+    "Wyłączono server bo jest unhandled promise rejections, np w adresie do mongoDB jest literówka.."
+  );
+  server.close(() => {
+    process.exit(1);
+    // cg czemu 1 to nie wiem
+  });
 });
